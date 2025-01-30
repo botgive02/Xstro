3ed0e0fe9d2705ad7d203d10b66dc3bc58acbfc4
botgive02 <numbergive044@gmail.com>
2025-01-30 11:44:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config({ path: 'config.env' });
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || '',
-  SUDO: process.env.SUDO || '',
+  SESSION_ID: process.env.SESSION_ID || 'XSTRO_76_95_00',
+  SUDO: process.env.SUDO || '923046316500',
   BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
   WARN_COUNT: process.env.WARN_COUNT || 3,
   TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
